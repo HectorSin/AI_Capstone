5b90796dba605273a426864341b2fc95277342ce
HectorSin <kkang15634@ajou.ac.kr>
2025-09-09 18:56:22 +0900

    기획 변경에 따른 다른 주석 작성

--- AI/test.py
+++ AI/test.py
@@ -1,31 +1,33 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def split_text(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    return text_splitter.split_text(text)
-
-# ai 모델 PoC
+import requests
+# ai 모델 MVP 구현
 
 """
-Langchain 활용하여 구현
+특정 카테고리를 입력 받아 그 카테고리에 대한 팟캐스트를 생성하는 모델을 구현
+데이터 수집 -> 보고서 형태로 정리 및 요약 -> 팟캐스트 대본 생성 -> STT 모델 활용 팟캐스트 생성 -> 음성파일 전처리리 -> 팟캐스트 리턴
 """
 
-# 텍스트 입력
-input_text = "3D 프린팅 기술"
+# 카테고리
+category = "VLLM, CV, LLM"
+# 데이터 수집
+## 해당하는 카테고리의 URL 수집
+url = f"https://www.google.com/search?q={category}"
+response = requests.get(url)
+data = response.text
+print(data)
 
-# 텍스트 분할
-text_chunks = split_text(input_text)
+## 해당하는 URL 데이터 수집
 
-# 핵심 요약
-summary = summarize_text(text_chunks)
+## LLM 입력 가능한 형태로 데이터 전처리
 
-# 임베딩 및 벡터 저장
+# 보고서 형태로 정리 및 요약
 
-# 팟캐스트 생성
+# 팟캐스트 대본 생성 [보고서를 토대로로 팟캐스트 대본 생성]
 
-# 요약 리포트
+# STT 모델 활용 팟캐스트 생성 [팟캐스트 대본을 토대로 팟캐스트 생성]
 
-# Q&A
+# 음성파일 전처리 [팟캐스트 생성 후 음성파일 전처리]
+
+# 팟캐스트 리턴 [음성파일 전처리 후 팟캐스트 리턴]
 
 if __name__ == "__main__":
-    print(text_chunks)
+    pass
